5c3263c9e6aa13b430467fb928a40e1ebd5cddde
andrew zhang <61163004+aspartate@users.noreply.github.com>
2021-11-17 13:33:35 -0500

    update commit

--- js/main.js
+++ js/main.js
@@ -1,19 +1,36 @@
 // Initialize variables to save the charts later
-let lineplot
+let lineplot_all, lineplot_MA
 let foo
 
-// Date parser to convert strings to date objects
-let parseDate = d3.timeParse("%Y-%m-%d");
+// Load all data into data array as promises
+let promises = [
+	d3.csv("data/state_dex_processed.csv"),
+	d3.csv('data/state_lockdowns.csv'),
+	d3.csv("data/state_lex_processed.csv")
+]
+Promise.all(promises).then(data => {
+	// Lineplot
+	lineplot_all = new LinePlotAll('lineplot-all', data[0], data[1], 'dex_savgol')
+	// lineplot_MA = new LinePlotOneState('lineplot-onestate', data, 'MA', 'dex')
 
-// (1) Load CSV data
-// 	(2) Convert strings to date objects
-// 	(3) Create new bar chart objects
-// 	(4) Create new are chart object
+	// Matrix
+	let myMatrixVis = new matrixVis('matrixDiv', data[2])
+	let groupNames = ["Name"];
+	let groupValues = ["name"];
 
-d3.csv("data/state_dex.csv").then(data => {
-	lineplot = new LinePlot('lineplot', data, 'dex')
-	lineplot = new LinePlot('lineplot', data, 'num_devices')
-});
+	d3.select("#selectButton")
+		.selectAll("myOptions")
+		.data(groupValues)
+		.enter()
+		.append("option")
+		.text((d,i) => groupNames[i])
+		.attr("value", (d => d));
+
+	d3.select("#selectButton").on("change", function(event, d) {
+		let selectedOption = d3.select(this).property("value")
+		myMatrixVis.wrangleData(selectedOption);
+	})
+})
 
 // Refresh page when window resized https://stackoverflow.com/questions/14915653/refresh-page-on-resize-with-javascript-or-jquery
 window.onresize = function(){ location.reload(); }
